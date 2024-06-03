48f39b7905cc208e8e34bf597b4a3fbfe8b3bc63
ronni söderberg <ronni.soderberg@gmail.com>
2024-06-03 22:58:27 +0200

    lägger till småskit kommentarer

--- Javascript/TaggListManipulering.js
+++ Javascript/TaggListManipulering.js
@@ -8,11 +8,23 @@ import convertToXlsx from './convert.js';
 
 
 function xlsxManipulering(filename) {
+    // Read the .xlsx file
     
+
+    //Number of rows in the sheet
     const table = XLSX.readFile(filename);
     const sheet = table.Sheets[table.SheetNames[0]];
+
+
+
     const data = XLSX.utils.sheet_to_json(sheet, {header: 1});
 
+    //Loop through the data and print the value 
+    for (let i = 7; i < data.length; i++) {
+
+        //Printar välrden på namnet
+        console.log(data[i][2]);
+    }
 
 
 
