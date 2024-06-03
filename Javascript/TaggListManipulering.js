c308c1667489ba3b65d3938efb267e6440051ee6
Ronni Soderberg <ronni.soderberg@gmail.com>
2024-06-03 16:04:50 +0200

    trying to read how many rows and colums in a row and coloum

--- Javascript/TaggListManipulering.js
+++ Javascript/TaggListManipulering.js
@@ -1,4 +1,27 @@
-import './convert.js';
-// import readXlsx from './convert.js';
+import XLSX from 'xlsx';
+import convertToXlsx from './convert.js';
 
-convertToXlsx('L070n087_SET_Export.xls', 'L070n087_SET_Export.xlsx');
+
+convertToXlsx('L070n087_EDE.xls', 'L070n087_EDE.xlsx');
+
+
+
+
+function readXlsx(filename) {
+    // Read the .xlsx file
+    const workbook = XLSX.readFile(filename);
+    
+
+    // Loop through all sheets
+    workbook.SheetNames.forEach(sheetName => {
+        let excelRowsObjArr = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[she]);
+        const sheet = workbook.Sheets[sheetName];
+        // Print the content of the first cell in each sheet
+        console.log(`Content of the first cell in sheet ${sheetName}: ${sheet['C8'].v}`);
+    });
+    
+
+}
+readXlsx('L070n087_EDE.xlsx');
+
+
--- Javascript/convert.js
+++ Javascript/convert.js
@@ -4,6 +4,7 @@ import path from 'path';
 
 // Function to convert .xls or .csv to .xlsx
 export default function convertToXlsx(inputFilename, outputFilename) {
+//  function convertToXlsx(inputFilename, outputFilename) {
     const ext = path.extname(inputFilename).toLowerCase();
     let workbook;
 
@@ -24,27 +25,17 @@ export default function convertToXlsx(inputFilename, outputFilename) {
     XLSX.writeFile(workbook, outputFilename);
 }
 
-// Function to read .xlsx file
-function readXlsx(filename) {
-    // Read the .xlsx file
-    const workbook = XLSX.readFile(filename);
 
-    // Loop through all sheets
-    workbook.SheetNames.forEach(sheetName => {
-        const sheet = workbook.Sheets[sheetName];
-        // Print the content of the first cell in each sheet
-        // console.log(`Content of the first cell in sheet ${sheetName}: ${sheet['A1'].v}`);
-        console.log("1");
-    });
-}
+
+
 
 // Convert the first .xls file
-convertToXlsx('L070n087_SET_Export.xls', 'L070n087_SET_Export.xlsx');
+// convertToXlsx('L070n087_SET_Export.xls', 'L070n087_SET_Export.xlsx');
 
-// Convert a CSV file
-convertToXlsx('example.csv', 'example.xlsx');
+// // Convert a CSV file
+// convertToXlsx('example.csv', 'example.xlsx');
 
-// Read the converted .xlsx files
-readXlsx('L070n087_SET_Export.xlsx');
-readXlsx('example.xlsx');
-readXlsx('L070n087_EDE.xlsx');
+// // Read the converted .xlsx files
+// readXlsx('L070n087_SET_Export.xlsx');
+// readXlsx('example.xlsx');
+// readXlsx('L070n087_EDE.xlsx');
